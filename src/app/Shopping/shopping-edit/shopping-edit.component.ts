84bd8642e3f734337684386f0f9dd659200faf60
Rohit-Bhargava <Rohit_Bhargava_Freelance@outlook.com>
2021-10-11 08:30:42 +0530

    version4

--- src/app/Shopping/shopping-edit/shopping-edit.component.ts
+++ src/app/Shopping/shopping-edit/shopping-edit.component.ts
@@ -1,9 +1,11 @@
 import {
   Component,
+  OnDestroy,
   OnInit,
-  ElementRef,
   ViewChild
 } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 import { Ingradient } from '../../shared/ingradient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
@@ -13,20 +15,54 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
-  @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  @ViewChild('f') slForm: NgForm;
+  subscription: Subscription;
+  editMode=false;
+  editedItemIndex: number;
+  editedItem: Ingradient;
 
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
+    this.subscription = this.slService.startedEditing
+    .subscribe(
+      (index: number) => {
+        this.editedItemIndex=index;
+        this.editMode=true;
+        this.editedItem=this.slService.getIngradient(index);
+        this.slForm.setValue({
+          name: this.editedItem.name,
+          amount: this.editedItem.amount
+        })
+      }
+    )
   }
 
-  onAddItem() {
-    const ingName = this.nameInputRef.nativeElement.value;
-    const ingAmount = this.amountInputRef.nativeElement.value;
-    const newIngradient = new Ingradient(ingName, ingAmount);
-    this.slService.addIngradient(newIngradient);
+  onSubmit(form: NgForm) {
+    const value = form.value;
+    const newIngradient = new Ingradient(value.name, value.amount);
+    if(this.editMode) {
+      this.slService.upgradeIngradient(this.editedItemIndex, newIngradient);
+    } else {
+      this.slService.addIngradient(newIngradient);
+    }
+    this.editMode = false;
+    form.reset()
+  }
+
+  onClear(){
+    this.slForm.reset();
+    this.editMode = false;
+  }
+
+  onDelete(){
+    this.slService.deleteIngradient(this.editedItemIndex);
+    this.onClear();
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe()
   }
 
 }
--- src/app/Shopping/shopping-list/shopping-list.service.ts
+++ src/app/Shopping/shopping-list/shopping-list.service.ts
@@ -1,8 +1,10 @@
 import { Ingradient } from 'src/app/shared/ingradient.model';
 import { EventEmitter } from '@angular/core';
+import { Subject } from 'rxjs';
 
 export class ShoppingListService {
   ingradientsChanged = new EventEmitter<Ingradient[]>();
+  startedEditing = new Subject<number>();
   private ingradients: Ingradient[] = [
     new Ingradient('Apples', 5),
     new Ingradient('Tomatoes', 10),
@@ -10,6 +12,10 @@ export class ShoppingListService {
 
   getIngraedients() {
     return this.ingradients.slice();
+  }
+
+  getIngradient(index: number){
+    return this.ingradients[index];
   }
 
   addIngradient(ingradient: Ingradient) {
@@ -24,4 +30,15 @@ export class ShoppingListService {
     this.ingradients.push(...ingradients);
     this.ingradientsChanged.emit(this.ingradients.slice());
   }
+
+  upgradeIngradient(index: number, newIngradient: Ingradient){
+    this.ingradients[index]= newIngradient;
+    this.ingradientsChanged.next(this.ingradients.slice());
+  }
+
+  deleteIngradient(index: number){
+    this.ingradients.splice(index, 1);
+    this.ingradientsChanged.next(this.ingradients.slice());
+  }
+
 }
