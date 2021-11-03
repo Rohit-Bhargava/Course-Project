4fc740bb47bbb0767318f91ac1044b2438d57f94
Rohit-Bhargava <Rohit_Bhargava_Freelance@outlook.com>
2021-11-03 15:54:32 +0530

    final

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -8,6 +8,7 @@ import { ShoppingListService } from './shopping-list.service';
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
+  
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
